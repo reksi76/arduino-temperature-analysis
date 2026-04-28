b7addf1f43783922bdce3fcfe0fad9d518432e39
Reksi M. Silaban <reksisilaban76@gmail.com>
2026-04-28 22:55:56 +0700

    feat: Moving Average (MA) analysis

--- src/analysis.py
+++ src/analysis.py
@@ -6,60 +6,87 @@ from matplotlib.animation import FuncAnimation
 import serial
 import time
 
-ser = serial.Serial('COM5', 9600, timeout = 1)
+try:
+    ser = serial.Serial('COM5', 9600, timeout = 1)
+    print('Sensor Connected!')
+except:
+    print('No Sensor Connected!')
+    exit()
 
 raw_data = deque(maxlen=100)
-smooth_data = deque(maxlen=100)
+
+# Eksponential Moving Average (EMA)
+ema_data = deque(maxlen=100)
+# Moving Average (MA)
+ma_data = deque(maxlen=100)
 N = 50
 
 alpha = 0.2
 last_smooth = None
+window_size = 5
 
 fig, ax = plt.subplots()
 
 def update(frame):
-	global last_smooth
+    global last_smooth 
 
-	line = ser.readline().decode(errors='ignore').strip()
-	match = re.search(r'\d+\.?\d+', line)
+    try:
+        line = ser.readline().decode(errors='ignore').strip()
 
-	if match:
-		value = float(match.group())
+    except serial.SerialException:
+        print('Sensor Disconnected!')
+        return
+    except Exception as e:
+        print(f'Error: {e}')
 
-		if not (20 < value < 40):
-			return
+    match = re.search(r'\d+\.?\d+', line)
+    if not match:
+        return
+    value = float(match.group())
 
-		raw_data.append(value)
+    if not (20 < value < 40):
+        return
 
-		if last_smooth is None:
-			smooth = value 
+    raw_data.append(value)
 
-		else:
-			smooth = alpha * value + (1-alpha) * last_smooth
+    if last_smooth is None:
+        smooth = value 
 
-		last_smooth = smooth
-		smooth_data.append(smooth)
+    else:
+        smooth = alpha * value + (1-alpha) * last_smooth
 
-		if abs(value - smooth) > 0.5:
-			print(f'Anomaly detected: {value}')
+    if len(raw_data) >= window_size:
+        ma = sum(list(raw_data)[-window_size:]) / window_size
+        ma_data.append(ma)
+    else:
+        ma_data.append(value)
 
-	for temperature in raw_data:
-		print(f'{temperature} °C')
 
-	ax.clear()
-	ax.plot(raw_data, label='Raw Data')
-	ax.plot(smooth_data, label='Smoothed data')
-	ax.set_ylabel('Temperature (°C)')
-	ax.legend()
-	ax.set_title('Real-time Temperature (Sensor DS180B20)')
-	ax.grid(True)
+    last_smooth = smooth
+    ema_data.append(smooth)
+
+    if abs(value - smooth) > 0.5:
+        print(f'Anomaly detected: {value}')
+
+    for temperature in raw_data:
+        print(f'{temperature} °C')
+
+    ax.clear()
+    ax.plot(raw_data, label='Raw Data')
+    ax.plot(ema_data, label='Eksponential Moving Average (EMA)')
+    ax.plot(ma_data, label='Moving Average (MA)')
+    ax.set_ylabel('Temperature (°C)')
+    ax.legend()
+    ax.set_title('Real-time Temperature (Sensor DS180B20)')
+    ax.grid(True)
 
 ani = FuncAnimation(fig, update, interval=200)
 plt.show()
 
 with open('data.txt', 'a') as file:
-	for d in raw_data:
-		file.write(f'{d}\n')
+    for d in raw_data:
+        file.write(f'{d}\n')
 
 print('\nProgram finished!')
+time.sleep(3)
 
